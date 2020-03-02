308f988701077fc66f9e1769c1ddf47bf99a7739
alexandremajda <majda.a@outlook.com>
2020-03-02 14:50:12 +0100

    [Cleanse] remove some overkilled variables

--- Space_Invaders/src/Ship.cpp
+++ Space_Invaders/src/Ship.cpp
@@ -5,28 +5,24 @@
 Ship::Ship(Space& p_space) : SpaceElement("ressources/ship.png"), space{p_space} {}
 
 void Ship::actualiseState() {
-    beingAcceleratedFront = sf::Keyboard::isKeyPressed(sf::Keyboard::Up);
-    beingAcceleratedFrontS = sf::Keyboard::isKeyPressed(sf::Keyboard::Z);
-    beingAcceleratedBack = sf::Keyboard::isKeyPressed(sf::Keyboard::Down);
-    beingAcceleratedBackS = sf::Keyboard::isKeyPressed(sf::Keyboard::S);
-    beingAcceleratedLeft = sf::Keyboard::isKeyPressed(sf::Keyboard::Left);
-    beingAcceleratedLeftS = sf::Keyboard::isKeyPressed(sf::Keyboard::Q);
-    beingAcceleratedRight = sf::Keyboard::isKeyPressed(sf::Keyboard::Right);
-    beingAcceleratedRightS = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
+    beingAcceleratedFront = sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Z);
+    beingAcceleratedBack = sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S);
+    beingAcceleratedLeft = sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q);
+    beingAcceleratedRight = sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D);
 }
 
 void Ship::update(float time) {
     actualiseState();
-    if (beingAcceleratedFront || beingAcceleratedFrontS) {
+    if (beingAcceleratedFront) {
         speed += {0.f, -ACCELERATION * time};
     }
-    if (beingAcceleratedBack || beingAcceleratedBackS) {
+    if (beingAcceleratedBack) {
         speed += {0.f, ACCELERATION * time};
     }
-    if (beingAcceleratedLeft || beingAcceleratedLeftS) {
+    if (beingAcceleratedLeft) {
         speed += {-ACCELERATION * time, 0.f};
     }
-    if (beingAcceleratedRight || beingAcceleratedRightS) {
+    if (beingAcceleratedRight) {
         speed += {ACCELERATION * time, 0.f};
     }
     speed -= speed * COEF_FROTTEMENTS * time;
