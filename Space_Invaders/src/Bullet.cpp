9713facf9e159884931a064f16c66914e68dfc25
alexandremajda <majda.a@outlook.com>
2020-03-03 14:23:34 +0100

    [Fix] bullet start from the ship

--- Space_Invaders/src/Bullet.cpp
+++ Space_Invaders/src/Bullet.cpp
@@ -1,15 +1,15 @@
 #include "Bullet.h"
 
-Bullet::Bullet(Coordinate const& p_position):
+Bullet::Bullet(Coordinate const& p_position) :
 SpaceElement("ressources/missile.png")
 {
     position = p_position;
-    speed = Vecteur{VITESSE, 0.f};
+    speed = Vecteur{VITESSE, 0.1f};
     type = ElementType::BULLET;
-}
+} 
 
 void Bullet::crashReaction(ElementType otherType)
 {
     if (otherType == type)
-        std::cout << "ok" << std::endl;
+        destruct = true;
 }
