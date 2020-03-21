47884d8ce72f8b29627fdbf2128100043907caab
Boris <boris.lepage@gmail.com>
2020-03-21 21:38:59 +0100

    [Convert] tab to space

--- Space_Invaders/src/Background.cpp
+++ Space_Invaders/src/Background.cpp
@@ -3,23 +3,23 @@
 Background::Background():
 SpaceElement("ressources/map3.png")
 {
-	sprite.setOrigin(0, -50);
-	// sprite.setPosition(0, 0);
+    sprite.setOrigin(0, -50);
+    // sprite.setPosition(0, 0);
     type = ElementType::OTHER;
     ACCELERATION = 2.f;
 }
 
 void Background::crashTest(SpaceElement& other) {
     if (other.type == type)
-		return;
+        return;
 }
 
 void Background::update(float time) {
-	auto halfSpriteX = sprite.getLocalBounds().width / 2.f;
+    auto halfSpriteX = sprite.getLocalBounds().width / 2.f;
 
-	if (position.x > (position.widthSpace + 250 - (halfSpriteX * 2))) {
-    	speed += {-ACCELERATION * time, 0.f};
-	} else {
-		speed -= speed * 2.f * time;
-	}
+    if (position.x > (position.widthSpace + 250 - (halfSpriteX * 2))) {
+        speed += {-ACCELERATION * time, 0.f};
+    } else {
+        speed -= speed * 2.f * time;
+    }
 }
