3c86a498fdeb8362759ac8f50ea612bfd6f9fe2f
alexandremajda <majda.a@outlook.com>
2020-03-25 23:03:39 +0100

    [Feature] Generate the sine wave for cruisers

--- src/Cruiser.cpp
+++ src/Cruiser.cpp
@@ -9,29 +9,10 @@ Ennemy(p_space, x, y, "ressources/ennemy2.png")
 }
 
 void Cruiser::update(float time) {
-//     actualiseState();
     if (!destruct) {
-        // std::cout << std::cos(std::acos(-1)/2*time) << std::endl;
-        // speed += {std::cos(std::acos(-1)/2*(-ACCELERATION * time)), 0.f};
-
-        // amplitude
-        auto A = 2;
-        // phase shift
-        // auto shift = 1;
-
-        // frequence, nb cycles (inclinaison de la sinusoide)
-        // auto f = 0.25;
-        for (int i = 0; i < 10; i++){
-            auto test = A * -sin(2 * M_PI * i * time)+1;
-        
-            speed += {-ACCELERATION * time, static_cast<float>(test)};
-
-        }
-        // auto accel = static_cast<float>(std::cos(std::acos(-1)/2* (-ACCELERATION * time)));
-        // if (position.y > position.getHeightSpace() / 2 && position.y > position.getHeightSpace() - 10)
-        // else
-            // speed += {-ACCELERATION * time, static_cast<float>(test)};
-        // speed += {static_cast<float>(-test), 0.f};
+        test++;
+        float result = amplitude * cos(test * M_PI * frequency * time);
+        speed += {-ACCELERATION * time, result};
         speed -= speed * COEF_FROTTEMENTS * time;
         destructOutOfScreen();
     }
