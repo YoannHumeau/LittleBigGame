df6653135bc7d9102d63f0eb0a6044f9486fc40d
alexandremajda <majda.a@outlook.com>
2020-03-25 16:01:59 +0100

    [Fix] bug correction on bullets undestroyed

--- include/Cruiser.h
+++ include/Cruiser.h
@@ -2,11 +2,15 @@
 #define CRUISER_H
 
 #include "Ennemy.h"
+#include <cmath>
 
 class Cruiser: public Ennemy
 {
     public:
         explicit Cruiser(Space& p_space, float x, float y);
+        virtual void update(float time) override;
+    private:
+        const double pi = std::acos(-1);
 };
 
 #endif
