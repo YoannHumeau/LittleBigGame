31396f6d600f2134916941e4e65d6c641adcf4e5
device <humeau_y@etna-alternance.net>
2020-03-26 14:02:11 +0100

    Sound design mother fucker :P

--- src/Cruiser.cpp
+++ src/Cruiser.cpp
@@ -5,7 +5,7 @@ Cruiser::Cruiser(Space &p_space, float x, float y):
 Ennemy(p_space, x, y, "ressources/ennemy2.png")
 {
     eType = EnnemyType::CRUISER;
-    ACCELERATION = 100.f;
+    ACCELERATION = 500.f;
 }
 
 void Cruiser::update(float time) {
